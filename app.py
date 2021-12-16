c03e2e5f3e2af194256dc6e494ecc21647efaa34
billmills <you@example.com>
2021-12-16 18:38:26 -0500

    tidy tidy

--- app.py
+++ app.py
@@ -17,13 +17,18 @@ def stripnum(text):
     if text is '':
         return float('NaN')
     try:
-        t = text.strip().strip('"').strip("'")
+        t = text.strip('\s\"\'')
+        if t[-1] == 'k':
+            return float(t[0:-1])*1000
+        if t[-1] == 'µ':
+            return float(t[0:-1])/1000000
         return float(t)
+
     except AttributeError:
         return text
 
 def layout():
-    df = pd.read_csv('/data/data.csv', sep=",", converters={0: strip, 1: strip, 2: stripnum})  
+    df = pd.read_csv('/data/xx.csv', sep=",", converters={0: strip, 1: strip, 2: stripnum})  
     df['Date']= pd.to_datetime(df['Date'], format= '%Y-%m-%dT%H:%M:%S.%f+00:00')
     print(df.columns[0])
     fig = px.line(df, x=df.columns[1], y=df.columns[2], color=df.columns[0], title=df.columns[1] + ' vs ' + df.columns[2] + ' per ' + df.columns[0])
